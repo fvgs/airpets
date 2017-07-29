2697be1f5b86b075cd8cf5f0c7d36160cb01f70a
Felipe Vargas <git@fvgs.ai>
2017-07-29 12:40:54 -0700

    Add background gradient

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Camera from './Camera.js';
+
+import logo from './logo.svg';
+import './App.css';
+import startBackgroundGradient from './startBackgroundGradient'
+
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {page: "index"};
     }
+  componentDidMount() {
+    startBackgroundGradient();
+  }
 
-    render() {
-        if (this.state.page === "index") {
-            return (
-                <div>
-                    <p>
-                        Index
-                    </p>
-                    <a onClick={() => this.setState({page: "camera"})}> Camera </a>
-                </div>
-            );
-        }
+  render() {
+    if (this.state.page === "index") {
         return (
-            <Camera/>
+          <div style={{height: '100vh'}} className="main">
+            <p>
+                Index
+            </p>
+            <a onClick={() => this.setState({page: "camera"})}> Camera </a>
+          </div>
         );
     }
+    return (
+      <Camera/>
+    );
+  }
 }
 
 export default App;
