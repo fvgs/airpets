a146a5e9afc5c25e786fb36bc63ae79eb84f5e8a
Charles Yuan <charlesyuan314@gmail.com>
2017-07-29 13:22:46 -0700

    box primitive

--- src/Camera.js
+++ src/Camera.js
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
+import 'aframe';
+import {Entity, Scene} from 'aframe-react';
 
 class Camera extends Component {
+    componentWillMount() {
+        const script = document.createElement("script");
+        script.src = "https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js";
+        script.async = true;
+        document.body.appendChild(script);
+    }
+
     render() {
         return (
-            <div className="Camera">
-                <p>
-                    Hello
-                </p>
-            </div>
+            <Scene arjs="sourceType: webcam;">
+                <Entity geometry={{primitive: 'box'}} material={{color: 'red'}} position={{x: 0, y: 0.5, z: 0}}/>
+                <a-marker-camera preset="hiro"/>
+            </Scene>
         );
     }
 }
