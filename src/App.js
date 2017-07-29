cb3439d28ef4893fc584340dcfb2cf4b96e15865
Charles Yuan <charlesyuan314@gmail.com>
2017-07-29 12:39:40 -0700

    add shitty camera page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Camera from './Camera.js';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {page: "index"};
+    }
+
+    render() {
+        if (this.state.page === "index") {
+            return (
+                <div>
+                    <p>
+                        Index
+                    </p>
+                    <a onClick={() => this.setState({page: "camera"})}> Camera </a>
+                </div>
+            );
+        }
+        return (
+            <Camera/>
+        );
+    }
 }
 
 export default App;
---
+++ src/Camera.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Camera extends Component {
+    render() {
+        return (
+            <div className="Camera">
+                <p>
+                    Hello
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Camera;
