36d08acafcf95fc00934d70d2c9dce4ff96c9ff6
Felipe Vargas <git@fvgs.ai>
2017-07-29 18:26:13 -0700

    Add socket.io client and make improvements

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,34 @@
 import React, { Component } from 'react';
 import Camera from './Camera.js';
+import io from 'socket.io-client'
 
 import './App.css';
 import Home from './Home'
 
 const styles = {
-	height: '100vh',
+  height: '100vh',
 }
 
 class App extends Component {
     constructor(props) {
-        super(props);
-        this.state = {page: "home"};
+      super(props);
+      this.state = {page: "home"};
+      this.socket = io('localhost:4000')
     }
 
-	changePage = (page) => {
-		this.setState({page});
-	}
+  changePage = (page) => {
+    this.setState({page});
+  }
 
   render() {
-		const {page} = this.state
+    const {page} = this.state
 
-		return (
-			<div style={styles}>
-				{page === 'home' && <Home changePage={this.changePage}/>}
-				{page === 'camera' && <Camera/>}
-			</div>
-		)
+    return (
+      <div style={styles}>
+        {page === 'home' && <Home changePage={this.changePage}/>}
+        {page === 'camera' && <Camera socket={this.socket}/>}
+      </div>
+    )
   }
 }
 
--- client/src/Camera.js
+++ client/src/Camera.js
@@ -5,10 +5,6 @@ import {Vector3} from 'three';
 
 const DURATION_MS = 20;
 
-function processPosition(pos) {
-    console.log(pos);
-}
-
 class Camera extends Component {
     constructor(props) {
         super(props);
@@ -23,27 +19,29 @@ class Camera extends Component {
     }
 
     render() {
-        return (
-            <Scene arjs='sourceType: webcam;' events={{
-                "child-attached": () => {
-                    if (!this.state.attached && document.querySelector("a-scene").camera) {
-                        setInterval(() => {
-                            const worldPos = new Vector3();
-                            worldPos.setFromMatrixPosition(document.querySelector("a-scene").camera.matrixWorld);
-                            processPosition(worldPos);
-                        }, DURATION_MS);
-                        this.setState({attached: true});
-                    }
-                }
-            }}>
-                <Entity>
-                    <a-animation attribute="rotation" easing="linear" dur="1000" to="0 360 0" repeat="indefinite"/>
-                    <Entity id="spinner" gltf-model="url(/models/FidgetSpinner.gltf)" position={{x: 0, y: 0, z: 0}}
-                            rotation="90 0 0" scale="0.25 0.25 0.25"/>
-                </Entity>
-                <a-marker-camera preset="hiro"/>
-            </Scene>
-        );
+      const {socket} = this.props
+
+      return (
+          <Scene arjs='sourceType: webcam;' events={{
+              "child-attached": () => {
+                  if (!this.state.attached && document.querySelector("a-scene").camera) {
+                      setInterval(() => {
+                          const worldPos = new Vector3();
+                          worldPos.setFromMatrixPosition(document.querySelector("a-scene").camera.matrixWorld);
+                          socket.emit('position update', worldPos)
+                      }, DURATION_MS);
+                      this.setState({attached: true});
+                  }
+              }
+          }}>
+              <Entity>
+                  <a-animation attribute="rotation" easing="linear" dur="1000" to="0 360 0" repeat="indefinite"/>
+                  <Entity id="spinner" gltf-model="url(/models/FidgetSpinner.gltf)" position={{x: 0, y: 0, z: 0}}
+                          rotation="90 0 0" scale="0.25 0.25 0.25"/>
+              </Entity>
+              <a-marker-camera preset="hiro"/>
+          </Scene>
+      );
     }
 }
 
