cc2740faf9500ab887e3102e64a0ab2f8f743df1
Felipe Vargas <git@fvgs.ai>
2017-07-30 00:00:45 -0700

    Improve Home page

--- client/src/Home/index.js
+++ client/src/Home/index.js
@@ -9,7 +9,22 @@ const styles = {
 	flexDirection: 'column',
 	alignItems: 'center',
 	justifyContent: 'center',
-}
+};
+
+const titleStyles = {
+	color: '#ffffff',
+	fontSize: '8rem',
+	marginBottom: '8rem',
+	fontFamily: 'Voltaire, sans-serif',
+};
+
+const buttonStyles = {
+	margin: '0.7rem',
+};
+
+const buttonContainerStyles = {
+	display: 'flex',
+};
 
 class Home extends Component {
   componentDidMount() {
@@ -19,7 +34,11 @@ class Home extends Component {
 	render() {
 		return (
 			<div style={styles} className="main">
-				<Button onClick={() => this.props.changePage('camera')}>Camera</Button>
+				<div style={titleStyles}>Airpets</div>
+				<div style={buttonContainerStyles}>
+					<Button secondary style={buttonStyles} size="big" onClick={() => this.props.changePage('camera')}>JOIN WORLD</Button>
+					<Button primary style={buttonStyles} size="big" onClick={() => {}}>CREATE WORLD</Button>
+			</div>
 			</div>
 		)
 	}
